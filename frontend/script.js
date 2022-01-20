d1fb4fc3ff3c3aff566e59053aaeed996c84df94
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-01-20 13:46:53 +0100

    Practising

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,56 @@
-function loadEvent() {
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+
+];
+
+*/
+const data = [
+
+    {
+        name: "Gergely Kiss",
+        favoriteFood:"csirke/rizs"
+    },
+    {
+        name: "Krisztián Bui",
+        favoriteFood:"banán"
+    },
+    {
+        name: "Buczkó Balázs",
+        favoriteFood:"kolbászos rájás pizza"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favoriteFood:"avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favoriteFood:"alma"
+    },
+    {
+        name: "Pörneczi Kriszián",
+        favoriteFood:"almáspite"
+    },
+]
+ function loadEvent() {
     console.log("Az oldal betöltődött")
 
-    const list = document.querySelectorAll("section")
-
-
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
+    const root = document.querySelector("#root")
+    
+    for (const frontendstudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendstudent.name}</h1>
+                <p>${frontendstudent.favoriteFood}</p>
+            </section>
+        
         `)
-        //elem.querySelector("h1").outerHTML = ""
         
     }
+  
 
 }
 window.addEventListener("load", loadEvent)
